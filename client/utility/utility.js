0e273567f5e29e2d9a924999bf6318d8b5c3e3db
Joseph Nagy <nagyjoseph9@gmail.com>
2021-11-03 16:25:18 -0400

    move to separate folder

---
+++ client/utility/utility.js
@@ -0,0 +1,44 @@
+/**
+ * ************************************
+ *
+ * @module  Utility 
+ * @author
+ * @date
+ * @description Utility functions to avoid cluttering global namespace 
+ *
+ * ************************************
+ */
+
+const utils = {}; 
+
+utils.getTokens = () => {
+    // object to return tokens in 
+    let tokens = {}; 
+
+    // get the url string via window object
+    let url = window.location.href; 
+
+    // search url for index of access token 
+    // this will be the start of 'important' part of url
+    let startIndex = url.search('access_token=');
+
+    // use index to get token 
+
+    // -> 'access_token=*token*'0
+    let accessToken = url.slice(startIndex).split('&')[0];
+    // -> 'refresh_token=*token*'
+    let refreshToken = url.slice(startIndex).split('&')[1];
+
+    // get actual tokens 
+    accessToken = accessToken.split('=')[1]; 
+    refreshToken = refreshToken.split('=')[1];
+
+    // put tokens in object to return
+    tokens['accessToken'] = accessToken; 
+    tokens['refreshToken'] = refreshToken;
+
+    return tokens; 
+}
+
+// EXPORT THE UTILITY HERE
+module.exports = utils;
